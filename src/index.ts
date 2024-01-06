764e145c36ba232476910be90c95748d5d8fb8e5
nikzz-tea <nikita.red638@gmail.com>
2024-01-06 06:22:18 +0300

    bancho auth handle

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Client } from 'tmi.js';
 import dotenv from 'dotenv';
 import { channel } from './config.json';
+import { BanchoClient } from 'bancho.js';
 
 dotenv.config();
 
-export const client = new Client({
+const client = new Client({
   connection: { reconnect: true },
   identity: {
     username: process.env.TWITCH_LOGIN,
@@ -13,11 +14,24 @@ export const client = new Client({
   channels: [channel],
 });
 
-client.on('connected', () => {
+export const bancho = new BanchoClient({
+  username: process.env.OSU_USERNAME as string,
+  password: process.env.OSU_SERVER_PASSWORD as string,
+});
+
+client.on('connected', async () => {
   let handler = require('./handlers/commandHandler');
   if (handler.default) handler = handler.default;
   handler(client);
-  console.log('connected');
+  console.log('TMI: connected');
+  try {
+    await bancho.connect();
+    console.log('Bancho: connected');
+  } catch (error) {
+    console.log(`Bancho: connection error\n${error}`);
+  }
 });
 
 client.connect();
+
+export default client;
