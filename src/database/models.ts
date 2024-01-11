35891cc6ccc351cdf1056cad6f037c19e035a2af
nikzz-tea <nikita.red638@gmail.com>
2024-01-11 17:42:50 +0300

    add level system

--- src/database/models.ts
+++ src/database/models.ts
@@ -1,4 +1,4 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 import sequelize from '.';
 
 export const Commands = sequelize.define(
@@ -10,12 +10,32 @@ export const Commands = sequelize.define(
   { timestamps: false, indexes: [{ unique: true, fields: ['name'] }] },
 );
 
-export const Users = sequelize.define(
-  'users',
+interface UserAttributes {
+  id: string;
+  exp: number;
+  lvl: number;
+}
+
+export class Users extends Model<UserAttributes> implements UserAttributes {
+  public id!: string;
+  public exp!: number;
+  public lvl!: number;
+}
+
+Users.init(
   {
-    id: { primaryKey: true, type: DataTypes.STRING },
-    exp: DataTypes.NUMBER,
-    lvl: DataTypes.NUMBER,
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    exp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    lvl: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
+    },
   },
-  { timestamps: false },
+  { sequelize, modelName: 'users', timestamps: false },
 );
---
+++ src/events/chat/updateExp.ts
@@ -0,0 +1,22 @@
+import { ChatUserstate } from 'tmi.js';
+import { Users } from '../../database/models';
+import { prefix } from '../../config.json';
+import parseMapId from '../../utils/parseMapId';
+import client from '../../api/twitch';
+
+export default async (
+  channel: string,
+  userstate: ChatUserstate,
+  message: string,
+  self: boolean,
+) => {
+  if (self) return;
+  if (message.startsWith(prefix)) return;
+  if (parseMapId(message)) return;
+  const [user] = await Users.findOrCreate({ where: { id: userstate['user-id'] } });
+  await user.increment('exp', { by: 10 });
+  if (user.exp < 10 + 500 * user.lvl) return;
+  await user.increment('lvl');
+  await user.update({ exp: 0 });
+  client.say(channel, `${userstate['display-name']} just reached level ${user.lvl + 1}! peepoClap`);
+};
